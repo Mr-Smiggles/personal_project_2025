930e458f62a5dc7acdd106f884e0de2d6eb5df0b
Jason Edwards <jasonwedwards09@gmail.com>
2025-03-13 09:26:10 -0500

    Should use a controller joysitck input to move motors clockwise, counter, and spin left and right in opposite directions

--- jcar.py
+++ jcar.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
+import pyglet
 from time import sleep
-from gpiozero import Motor
 
 #the following code is taken from https://github.com/Cokoino/CKK0018/blob/main/Tutorial/Code/Drive_4_motors_run.py
 #my current theory is that these pin setups don't directly controll the motors, rather the motor controller chips on the hat.
@@ -18,6 +18,19 @@ AN22 = 25
 BN21 = 26
 BN22 = 16
 temp1=1
+
+#setup controller
+controllers = pyglet.input.get_controllers()
+
+if controllers:
+    controller = controllers[0]
+    controller.open()
+
+while controller == True:
+    x = controller_instance.leftx
+    y = controller_instance.lefty
+
+
 
 GPIO.setmode(GPIO.BCM)
 #Define pin as output signal
@@ -44,16 +57,60 @@ p1=GPIO.PWM(NSLEEP1,1000)#Define p1 as a pulse signal of 1000 Hz
 p2=GPIO.PWM(NSLEEP2,1000)#Define p2 as a pulse signal of 1000 Hz
 
 #p1 and p2 control motor speeds. i guess two motors are always the same?
-p1.start(100)#P1 defaults to a duty cycle of 30%
+p1.start(30)#P1 defaults to a duty cycle of 30%
 p2.start(30)#P2 defaults to a duty cycle of 30%
 
-#this should make all motors move 'foward' (needs to be correctly defined later). two should move at full speed and the other to at 30%
-GPIO.output(AN11,GPIO.LOW)
-GPIO.output(AN12,GPIO.HIGH)
-GPIO.output(BN11,GPIO.LOW)
-GPIO.output(BN12,GPIO.HIGH)
-GPIO.output(AN21,GPIO.LOW)
-GPIO.output(AN22,GPIO.HIGH)
-GPIO.output(BN21,GPIO.LOW)
-GPIO.output(BN22,GPIO.HIGH)
-temp1=0
+#the following code assumes that a negative voltage cannot be applied to spin the motor in the opposite direction
+
+#this if statement should take care of foward and reversal of the motors with the joystick
+
+while controller == True:
+    if y >= 0:
+        GPIO.output(AN11,GPIO.LOW)
+        GPIO.output(AN12,GPIO.HIGH)
+        GPIO.output(BN11,GPIO.LOW)
+        GPIO.output(BN12,GPIO.HIGH)
+        GPIO.output(AN21,GPIO.LOW)
+        GPIO.output(AN22,GPIO.HIGH)
+        GPIO.output(BN21,GPIO.LOW)
+        GPIO.output(BN22,GPIO.HIGH)
+        p1.ChangeDutyCycle(y)#Set the P1 pulse signal duty cycle to the value of y joystick%
+        p2.ChangeDutyCycle(y)#Set the P2 pulse signal duty cycle to y joystick%
+
+    elif y < 0:
+        GPIO.output(AN11,GPIO.HIGH)
+        GPIO.output(AN12,GPIO.LOW)
+        GPIO.output(BN11,GPIO.HIGH)
+        GPIO.output(BN12,GPIO.LOW)
+        GPIO.output(AN21,GPIO.HIGH)
+        GPIO.output(AN22,GPIO.LOW)
+        GPIO.output(BN21,GPIO.HIGH)
+        GPIO.output(BN22,GPIO.LOW)
+        p1.ChangeDutyCycle(-y)#Set the P1 pulse signal duty cycle to the value of y joystick%
+        p2.ChangeDutyCycle(-y)#Set the P2 pulse signal duty cycle to y joystick%
+
+    #the follwoing code should spin the left & right motors opposite with the x joystick value
+    if x >= 0:
+        GPIO.output(AN11,GPIO.LOW)
+        GPIO.output(AN12,GPIO.HIGH)
+        GPIO.output(BN11,GPIO.HIGH)
+        GPIO.output(BN12,GPIO.LOW)
+        GPIO.output(AN21,GPIO.LOW)
+        GPIO.output(AN22,GPIO.HIGH)
+        GPIO.output(BN21,GPIO.HIGH)
+        GPIO.output(BN22,GPIO.LOW)
+        p1.ChangeDutyCycle(x)#Set the P1 pulse signal duty cycle to the value of x joystick%
+        p2.ChangeDutyCycle(x)#Set the P2 pulse signal duty cycle to x joystick%
+
+    elif x < 0:
+        GPIO.output(AN11,GPIO.HIGH)
+        GPIO.output(AN12,GPIO.LOW)
+        GPIO.output(BN11,GPIO.LOW)
+        GPIO.output(BN12,GPIO.HIGH)
+        GPIO.output(AN21,GPIO.HIGH)
+        GPIO.output(AN22,GPIO.LOW)
+        GPIO.output(BN21,GPIO.LOW)
+        GPIO.output(BN22,GPIO.HIGH)
+        p1.ChangeDutyCycle(-x)#Set the P1 pulse signal duty cycle to the value of x joystick%
+        p2.ChangeDutyCycle(-x)#Set the P2 pulse signal duty cycle to the value x joystick%
+
