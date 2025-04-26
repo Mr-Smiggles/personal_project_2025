1b53f384a0a7caa5fb46e5c6bbee9797a8044245
Jason <jasonwedwards09@gmail.com>
2025-04-26 17:49:47 -0500

    Changing gpio source

--- buttonTest.py
+++ buttonTest.py
@@ -1,8 +1,6 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+from gpiozero import Button
 
-while True: # Run forever
-    if GPIO.input(2) == GPIO.HIGH:
-        print("Button was pushed!")
+button.wait_for_press(2)
+
+button.wait_for_press()
+print("Button pressed")
