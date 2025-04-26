56f693a5510d65b7660430d9f6e601e61466b451
Jason <jasonwedwards09@gmail.com>
2025-04-26 14:41:16 -0500

    button debugging

--- buttonTest.py
+++ buttonTest.py
@@ -9,8 +9,9 @@ GPIO.setup(output,GPIO.OUT)
 GPIO.setup(redInput,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while running:
+    print(f"Pin state: {GPIO.input(redInput)}")
     if GPIO.input(redInput) == GPIO.HIGH:
-        print("Button Pushed!", exit='\n')
+        print("Button Pushed!")
     else:
         GPIO.output(output,GPIO.LOW)
         print("Button Not Pushed!")
