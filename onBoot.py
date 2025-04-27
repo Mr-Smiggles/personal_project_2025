6df86dd0169382e42f92ccec507c01f8d0dff690
Jason <jasonwedwards09@gmail.com>
2025-04-27 18:43:30 -0500

    ts pmo icl

--- onBoot.py
+++ onBoot.py
@@ -1,17 +1,26 @@
 import os
-
-output = os.popen('ps -aux | grep python').read()
-
-
-if 'buttons.py' not in output:
-    os.system('python /home/jason/personal_project_2025/buttons.py')
+from time import sleep
+from gpiozero import Button
 
 
-def restartJCar():
-    global output
-    if 'jcar.py' not in output:
-        os.system('python /home/jason/personal_project_2025/jcar.py')
+redButton = Button(21)
+yellowButton = Button(20)
+greenButton = Button(18)
 
+os.system('python3 /home/jason/personal_project_2025/jcar.py')
 
 while True:
-    restartJCar()
+    if redButton.is_pressed:
+        os.system('pkill -f jcar.py')
+        print("Red button pressed")
+        os.system('sudo shutdown now')
+    if yellowButton.is_pressed:
+        os.system('pkill -f jcar.py')
+        print("Yellow button pressed")
+    if greenButton.is_pressed:
+        os.system('pkill -f jcar.py')
+        print("Green button pressed")
+        sleep(1)
+        os.system('python /home/jason/personal_project_2025/jcar.py')
+    else:
+        print("Button is not pressed")
