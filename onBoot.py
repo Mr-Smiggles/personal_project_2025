621827cd72edb55a031288109018b8b758457c6c
Jason <jasonwedwards09@gmail.com>
2025-04-27 13:43:08 -0500

    GPIO cleanup attempt #1

--- onBoot.py
+++ onBoot.py
@@ -1,15 +1,33 @@
 import os
+import RPi.GPIO as GPIO
 from time import sleep
 from gpiozero import Button
 
-
+clean = False
 redButton = Button(21)
 yellowButton = Button(20)
 greenButton = Button(16)
 
 os.system('python /home/jason/personal_project_2025/jcar.py')
 
+def gpioCleanup():
+    output = os.popen('ps -aux | grep python').read()
+    if 'jcar.py' in output:
+        print("jcar.py is running", end='\r')
+    else:
+        GPIO.cleanup()
+        clean = True
+
 while True:
+    gpioCleanup()
+    if clean:
+        redButton = Button(21)
+        yellowButton = Button(20)
+        greenButton = Button(16)
+        clean = False
+    else:
+        continue
+
     if redButton.is_pressed:
         os.system('pkill -f jcar.py')
         os.system('sudo shutdown now')
