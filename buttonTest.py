eeb23acb91e7691b2f357a8a20b3e816c973a794
Jason <jasonwedwards09@gmail.com>
2025-04-27 15:17:01 -0500

    debugging buttons, trying when_pressed in buttonTest.py

--- buttonTest.py
+++ buttonTest.py
@@ -5,13 +5,25 @@ redButton = Button(21)
 yellowButton = Button(20)
 greenButton = Button(18)
 
+def redButtonPressed():
+    print("Red button pressed")
+
+def yellowButtonPressed():
+    print("Yellow button pressed")
+
+def greenButtonPressed():
+    print("Green button pressed")
+# while True:
+#     if redButton.is_pressed:
+#         print("Red button pressed")
+#     if yellowButton.is_pressed:
+#         print("Yellow button pressed")
+#     if greenButton.is_pressed:
+#         print("Green button pressed")
+#     else:
+#         pass
 
 while True:
-    if redButton.is_pressed:
-        print("Red button pressed")
-    if yellowButton.is_pressed:
-        print("Yellow button pressed")
-    if greenButton.is_pressed:
-        print("Green button pressed")
-    else:
-        pass
+    redButton.when_pressed = redButtonPressed
+    yellowButton.when_pressed = yellowButtonPressed
+    greenButton.when_pressed = greenButtonPressed
