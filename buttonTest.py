f9ebdb4e466489286948cfaa7b66f833de521520
Jason <jasonwedwards09@gmail.com>
2025-04-27 12:52:20 -0500

    debug

--- buttonTest.py
+++ buttonTest.py
@@ -1,18 +1,20 @@
 from gpiozero import Button
 
-i = 0
 
 redButton = Button(21)
 yellowButton = Button(20)
 greenButton = Button(16)
 
+def n():
+    n += 1
+    return n
 
 while True:
     if redButton.is_pressed:
-        print("Red button pressed", i = i+1)
+        print("Red button pressed", n())
     if yellowButton.is_pressed:
-        print("Yellow button pressed", i = i+1)
+        print("Yellow button pressed", n())
     if greenButton.is_pressed:
-        print("Green button pressed", i = i+1)
+        print("Green button pressed", n())
     else:
         pass
