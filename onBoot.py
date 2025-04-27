91de46e0ed6e81706197cdf2c03f607b2ee5209f
Jason <jasonwedwards09@gmail.com>
2025-04-27 15:21:19 -0500

    debugging buttons, adding when_pressed to onBoot.py

--- onBoot.py
+++ onBoot.py
@@ -12,22 +12,33 @@ def restartJCar():
     output = os.popen('ps -aux | grep python').read()
     if 'jcar.py' not in output and doNotRestart == False:
         os.system('python /home/jason/personal_project_2025/jcar.py')
+
+def redButtonPressed():
+    global doNotRestart
+    doNotRestart = True
+    print("Red button pressed")
+    os.system('pkill -f jcar.py')
+    os.system('sudo shutdown now')
+
+def yellowButtonPressed():
+    global doNotRestart
+    doNotRestart = True
+    print("Yellow button pressed")
+    os.system('pkill -f jcar.py')
+
+def greenButtonPressed():
+    global doNotRestart
+    doNotRestart = True
+    print("Green button pressed")
+    os.system('pkill -f jcar.py')
+    sleep(5)
+    doNotRestart = False
  
+def buttonPressed():
+    redButton.when_pressed = redButtonPressed
+    yellowButton.when_pressed = yellowButtonPressed
+    greenButton.when_pressed = greenButtonPressed
 
 while True:
     restartJCar()
-    
-    if redButton.is_pressed:
-        doNotRestart = True
-        os.system('pkill -f jcar.py')
-        os.system('sudo shutdown now')
-    if yellowButton.is_pressed:
-        doNotRestart = True
-        os.system('pkill -f jcar.py')
-    if greenButton.is_pressed:
-        doNotRestart = True
-        os.system('pkill -f jcar.py')
-        sleep(5)
-        doNotRestart = False
-    sleep(1)
-   
+    buttonPressed()   
