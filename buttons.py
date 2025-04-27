0146b62cb0c11ba24046a9b1692ec0eb1bfd976a
Jason <jasonwedwards09@gmail.com>
2025-04-27 15:52:27 -0500

    added buttons.py

---
+++ buttons.py
@@ -0,0 +1,35 @@
+from gpiozero import Button
+from time import sleep
+import os
+
+
+redButton = Button(21)
+yellowButton = Button(20)
+greenButton = Button(18)
+
+
+def redButtonPressed():
+    print("Red button pressed")
+    os.system('pkill -f onBoot.py')
+    os.system('pkill -f jcar.py')
+    os.system('sudo shutdown now')
+
+def yellowButtonPressed():
+    print("Yellow button pressed")
+    os.system('pkill -f onBoot.py')
+    os.system('pkill -f jcar.py')
+
+def greenButtonPressed():
+    print("Green button pressed")
+    os.system('pkill -f onBoot.py')
+    os.system('pkill -f jcar.py')
+    sleep(5)
+    os.system('python /home/jason/personal_project_2025/onBoot.py')
+ 
+def buttonPressed():
+    redButton.when_pressed = redButtonPressed
+    yellowButton.when_pressed = yellowButtonPressed
+    greenButton.when_pressed = greenButtonPressed
+
+while True:
+    buttonPressed()
--- onBoot.py
+++ onBoot.py
@@ -1,44 +1,17 @@
 import os
-from time import sleep
-from gpiozero import Button
 
-doNotRestart = False
-redButton = Button(21)
-yellowButton = Button(20)
-greenButton = Button(18)
+output = os.popen('ps -aux | grep python').read()
+
+
+if 'buttons.py' not in output:
+    os.system('python /home/jason/personal_project_2025/buttons.py')
 
 
 def restartJCar():
-    output = os.popen('ps -aux | grep python').read()
-    if 'jcar.py' not in output and doNotRestart == False:
+    global output
+    if 'jcar.py' not in output:
         os.system('python /home/jason/personal_project_2025/jcar.py')
 
-def redButtonPressed():
-    global doNotRestart
-    doNotRestart = True
-    print("Red button pressed")
-    os.system('pkill -f jcar.py')
-    os.system('sudo shutdown now')
-
-def yellowButtonPressed():
-    global doNotRestart
-    doNotRestart = True
-    print("Yellow button pressed")
-    os.system('pkill -f jcar.py')
-
-def greenButtonPressed():
-    global doNotRestart
-    doNotRestart = True
-    print("Green button pressed")
-    os.system('pkill -f jcar.py')
-    sleep(5)
-    doNotRestart = False
- 
-def buttonPressed():
-    redButton.when_pressed = redButtonPressed
-    yellowButton.when_pressed = yellowButtonPressed
-    greenButton.when_pressed = greenButtonPressed
 
 while True:
     restartJCar()
-    buttonPressed()   
