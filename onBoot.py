997f866038ac1fa4cf1f236ac99582a2256998eb
Jason <jasonwedwards09@gmail.com>
2025-04-27 14:53:55 -0500

    debugging buttons

--- onBoot.py
+++ onBoot.py
@@ -2,27 +2,30 @@ import os
 from time import sleep
 from gpiozero import Button
 
+doNotRestart = False
 redButton = Button(21)
 yellowButton = Button(20)
-greenButton = Button(16)
+greenButton = Button(18)
 
-os.system('python /home/jason/personal_project_2025/jcar.py')
 
 def restartJCar():
     output = os.popen('ps -aux | grep python').read()
-    if 'jcar.py' not in output:
+    if 'jcar.py' not in output and doNotRestart == False:
         os.system('python /home/jason/personal_project_2025/jcar.py')
 
 def buttonPressed():
     if redButton.is_pressed:
+        doNotRestart = True
         os.system('pkill -f jcar.py')
         os.system('sudo shutdown now')
     if yellowButton.is_pressed:
+        doNotRestart = True
         os.system('pkill -f jcar.py')
     if greenButton.is_pressed:
+        doNotRestart = True
         os.system('pkill -f jcar.py')
-        sleep(1)
-        os.system('python /home/jason/personal_project_2025/jcar.py')
+        sleep(5)
+        doNotRestart = False
  
 
 while True:
